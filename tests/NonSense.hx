add22d0f72364a4e9b3813e5ef534924da23db4c
Juraj Kirchheim <back2dos@googlemail.com>
2019-03-10 18:24:50 +0100

    OCD.

--- tests/NonSense.hx
+++ tests/NonSense.hx
@@ -1,9 +1,9 @@
 import Tags.*;
 
 class NonSense {
-  static public function showOff() {
+  static public function showOff() 
     Plain.hxx(
-      <💩💩💩 🔫🚀🦖="🏃!!!!" /> //shit, shit, shit!!! laser rocket dinosaurs ... run!!!!
-    );    
-  }
+      <💩💩💩 🔫🚀🦖="🏃!!!!" /> 
+      //shit, shit, shit!!! laser rocket dinosaurs ... run!!!!
+    );
 }
--- tests/Tags.hx
+++ tests/Tags.hx
@@ -1,6 +1,5 @@
 class Tags {
-
   @:hxx('💩💩💩') 
   static public function div(attr:{ @:hxx('🔫🚀🦖') @:optional var foo:String; }, ?children:Array<{}>)
-    return children;    
+    return children;
 }
